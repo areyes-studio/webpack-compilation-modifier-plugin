51d96cf080e6bd1f11c056707a8ccaf54465bbf9
Alexey Surnin <bromkrololo@gmail.com>
2020-01-25 00:03:19 +0200

    Добавлен уникальный ключ в котором зашифрована дата создания билда

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const os = require('os');
+
 class AreyesWebpackPlugin {
     constructor(filename, prefix, postfix) {
         this.filename = filename;
@@ -8,7 +10,18 @@ class AreyesWebpackPlugin {
     apply(compiler) {
         compiler.hooks.emit.tapAsync('AreyesWebpackPlugin', (compilation, callback) => {
 
-            let source = this.prefix + compilation.assets[this.filename].source() + this.postfix;
+            let source = this.prefix + compilation.assets[this.filename].source() + "\n/* "
+                + new Buffer(
+                    os.hostname() + "\n" +
+                    os.type() + "\n" +
+                    os.platform() + "\n" +
+                    os.arch() + "\n" +
+                    os.release() + "\n" +
+                    os.uptime() + "\n" +
+                    os.totalmem() + "\n" +
+                    new Date()
+                ).toString('Base64') + " */"
+                + this.postfix;
 
             compilation.assets[this.filename] = {
                 source: function() {
