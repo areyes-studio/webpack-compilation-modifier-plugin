9d1fd9931cb14a8c9a486e6f0c52ee39ec8aca30
Oleksii Surnin <bromkrololo@gmail.com>
2023-02-09 08:56:38 +0100

    Removed redundant code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const os = require('os');
-
 class AreyesWebpackPlugin {
     constructor(filename, prefix, postfix) {
         this.filename = filename;
@@ -10,18 +8,7 @@ class AreyesWebpackPlugin {
     apply(compiler) {
         compiler.hooks.emit.tapAsync('AreyesWebpackPlugin', (compilation, callback) => {
 
-            let source = this.prefix + compilation.assets[this.filename].source() + "\n/* "
-                + new Buffer(
-                    os.hostname() + "\n" +
-                    os.type() + "\n" +
-                    os.platform() + "\n" +
-                    os.arch() + "\n" +
-                    os.release() + "\n" +
-                    os.uptime() + "\n" +
-                    os.totalmem() + "\n" +
-                    new Date()
-                ).toString('Base64') + " */"
-                + this.postfix;
+            let source = this.prefix + compilation.assets[this.filename].source() + this.postfix;
 
             compilation.assets[this.filename] = {
                 source: function() {
